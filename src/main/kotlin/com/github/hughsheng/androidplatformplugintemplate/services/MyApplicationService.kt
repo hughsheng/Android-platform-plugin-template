98c8fc45c9a1bb81adbcb3a1b688c6acacb9306b
GitHub Action <action@github.com>
2021-10-11 09:29:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hughsheng/androidplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hughsheng.androidplatformplugintemplate.services
+
+import com.github.hughsheng.androidplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
